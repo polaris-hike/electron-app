7fbd27e45c229e16047814be9813e69bb428f853
wuxuwei <2509165479@qq.com>
2021-08-24 22:40:21 +0800

    init

---
+++ app/main/electron.ts
@@ -0,0 +1,21 @@
+const path = require('path');
+const { app, BrowserWindow}  = require('electron');
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  mainWindow.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow();
+  app.on('activate',function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  })
+})
